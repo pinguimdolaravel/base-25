9d6a017adf6119a0a843bc6dbd4f721ed92fd207
Rafael Lunardelli <rafael@lunardelli.me>
2025-05-29 16:33:04 -0600

    Add email masking for deleted users and implement DestroyUser test

---
+++ tests/Feature/Livewire/Users/DestroyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Brain\User\Tasks\DestroyUser;
+use App\Livewire\Users\Destroy;
+use Illuminate\Support\Facades\Bus;
+use Livewire\Livewire;
+
+it('should check if the task is being called', function () {
+    Bus::fake([DestroyUser::class]);
+    $user = \App\Models\User::factory()->create();
+
+    Livewire::test(Destroy::class)
+        ->call('open', $user->id)
+        ->call('handle');
+
+    Bus::assertDispatched(DestroyUser::class);
+});
